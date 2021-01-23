f19917793dcfe522a268e4dda6ee758dadb39621
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2021-01-23 16:12:53 -0500

    add pagination

--- client/src/components/news/News.tsx
+++ client/src/components/news/News.tsx
@@ -2,34 +2,42 @@ import './News.scss'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ReactMarkdown from 'react-markdown'
+import ReactPaginate from 'react-paginate'
 import { getAllNewsFile } from '../../services/news/NewsService'
 
 type NewsPageProps = {}
+const PER_PAGE = 10
 
 const NewsPage: React.FunctionComponent<NewsPageProps> = (): any => {
   const { t, i18n } = useTranslation('news')
   const [filesNews, setFilesNews] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
   const [selectedFile, setSelectedFile] = useState()
 
   useEffect(() => {
     const filesNews = async () => {
       const response = await getAllNewsFile(i18n.language)
       setFilesNews(response.news)
-      if(response.news.length > 0)
-        setSelectedFile(response.news[0])
+      if (response.news.length > 0) setSelectedFile(response.news[0])
     }
     filesNews()
   }, [i18n.language])
 
-  function handleClickNews(
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) {
-    event.stopPropagation()
-    event.preventDefault()
-    const key = (event.target as HTMLDivElement).getAttribute('data-key')
-    console.log()
-    setSelectedFile(filesNews[+key-1])
+  // function handleClickNews(
+  //   event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  // ) {
+  //   event.stopPropagation()
+  //   event.preventDefault()
+  //   const key = (event.target as HTMLDivElement).getAttribute('data-key')
+  //   setSelectedFile(filesNews[+key - 1])
+  // }
+
+  function handlePageClick(selectedPage: { selected: number }) {
+    setCurrentPage(selectedPage.selected)
+    console.log(selectedPage)
+    setSelectedFile(filesNews[+selectedPage.selected])
   }
+  const pageCount = filesNews.length
 
   return (
     <div className="news-page-container">
@@ -41,9 +49,21 @@ const NewsPage: React.FunctionComponent<NewsPageProps> = (): any => {
         </div>
       </header>
       <main>
-        <div>{filesNews.map((file, index) => {
-          return (<div key={index+1} data-key={index+1} onClick={handleClickNews}>News {index+1}</div>)
-        })}</div>
+        <div>
+          <ReactPaginate
+            previousLabel={'← Previous'}
+            nextLabel={'Next →'}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination'}
+            previousLinkClassName={'pagination__link'}
+            nextLinkClassName={'pagination__link'}
+            disabledClassName={'pagination__link--disabled'}
+            activeClassName={'pagination__link--active'}
+            pageRangeDisplayed={5}
+            marginPagesDisplayed={5}
+          />
+        </div>
         <div className="news-body">
           <ReactMarkdown
             className="news"
