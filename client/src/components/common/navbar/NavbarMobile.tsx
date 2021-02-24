d9dbf05863ba8f5b79560963fc4da0c07ff8dd75
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2021-02-23 20:25:49 -0500

    fix styling

--- client/src/components/common/navbar/NavbarMobile.tsx
+++ client/src/components/common/navbar/NavbarMobile.tsx
@@ -42,7 +42,7 @@ const NavbarMobile: React.FunctionComponent<NavbarMobileProps> = (
           <div className="-mr-2 flex md:hidden">
             <button
               onClick={() => handleClick()}
-              className="mr-3 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="mr-3 bg-gray-800 p-1 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
             >
               <span className="sr-only">Change Language</span>
               <div className="h-6 w-8">{lang.toUpperCase()}</div>
@@ -50,7 +50,7 @@ const NavbarMobile: React.FunctionComponent<NavbarMobileProps> = (
             {/* <!-- Mobile menu button --> */}
             <button
               onClick={() => handleOpenMenu()}
-              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
             >
               <span className="sr-only">Open main menu</span>
               {/* <!--
