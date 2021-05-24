7e4ed84b04b43db3eee1998480a682bf4de73d32
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2021-05-23 20:52:20 -0400

    use es11 coala null

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import { Logger } from "./logger/logger";
 import Routes from "./routes/routes";
 
 class App {
 
     public express: express.Application;
+    public logger: Logger
 
     // array to hold users
     public users: any[];
 
     constructor() {
+        this.logger = new Logger();
         this.express = express();
         this.middleware();
         this.routes();
@@ -25,7 +28,9 @@ class App {
     private routes(): void {
         // user route
         this.express.use("/api", Routes);
-
+        const a = undefined;
+        const b = a ?? 'test';
+        this.logger.info(b)
         // handle undefined routes
         this.express.use("*", (req, res, next) => {
             res.send("Make sure url is correct!!!");
