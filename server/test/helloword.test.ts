8498fc9b0a6bfd0908c234356a11b5b4c297e258
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2021-07-01 21:53:03 -0400

    add first unit test

---
+++ server/test/helloword.test.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import HelloWord from "../src/routes/hello-word";
+
+describe("GET /api", () => {
+    it("should return 200 OK", () => {
+        return request(HelloWord).get("/test")
+            .expect(200);
+    });
+});
